fcb065d3a6f5c00ad5891887d7ed1921e4c5c42d
savazeb <44660901+savazeb@users.noreply.github.com>
2020-09-02 17:28:19 +0900

    Update PID.cpp

--- c/PID.cpp
+++ c/PID.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 class PID{
     private:
-    void init(double P, double I, double D, double SetPoint, double current_time);
+    void init(double P, double I, double D, double SetPoint);
     void clear();
     double update(double feedback_value , double current_time);
     void setSampleTime(double sample_time);
@@ -13,8 +13,10 @@ class PID{
     double Kp;
     double Ki;
     double Kd;
+    double current_time;
     double sample_time;
     double last_time;
+    double SetPoint_temp;
     double PTerm;
     double ITerm;
     double DTerm;
@@ -28,7 +30,8 @@ class PID{
     
 };
 
-void PID::init(double P, double I, double D, double SetPoint, double current_time){
+void PID::init(double P, double I, double D, double SetPoint){
+    
     Kp = P;
     Ki = I;
     Kd = D;
@@ -36,10 +39,25 @@ void PID::init(double P, double I, double D, double SetPoint, double current_tim
     sample_time = 0.00;
     
     time_t ct;
+    
     current_time = ct;
     last_time = current_time;
     
+    SetPoint_temp = SetPoint;
+    
+    clear();
 }
+
+void PID::clear(){
+    
+    PTerm = 0.00;
+    ITerm = 0.00;
+    DTerm = 0.00;
+
+    
+}
+
+
 
 int main()
 {
