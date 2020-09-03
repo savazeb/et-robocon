ba2c05e4814642798f6e0dfe7361ce1770a204ad
savazeb <44660901+savazeb@users.noreply.github.com>
2020-09-03 15:49:16 +0900

    Update main.cpp

--- c/main.cpp
+++ c/main.cpp
@@ -1,11 +1,15 @@
+/**
+ * This sample program balances a two-wheeled Segway type robot such as Gyroboy in EV3 core set.
+ *
+ * References:
+ * http://www.hitechnic.com/blog/gyro-sensor/htway/
+ * http://www.cs.bgu.ac.il/~ami/teaching/Lejos-2013/classes/src/lejos/robotics/navigation/Segoway.java
+ */
+
 #include "ev3api.h"
 #include "app.h"
 
-#if defined(BUILD_MODULE)
-#include "module_cfg.h"
-#else
-#include "kernel_cfg.h"
-#endif
+#include "libcpp-test.h"
 
 #define DEBUG
 
@@ -27,15 +31,26 @@ static const motor_port_t
 
 void main_task(intptr_t unused)
 {
+    /* センサー入力ポートの設定 */
+    ev3_sensor_config(sonar_sensor, ULTRASONIC_SENSOR);
+    ev3_sensor_config(color_sensor, COLOR_SENSOR);
+    ev3_color_sensor_get_reflect(color_sensor); /* 反射率モード */
+    ev3_sensor_config(touch_sensor, TOUCH_SENSOR);
+    /* モーター出力ポートの設定 */
     ev3_motor_config(left_motor, LARGE_MOTOR);
     ev3_motor_config(right_motor, LARGE_MOTOR);
     ev3_led_set_color(LED_ORANGE);
     while(1)
     { 
-        ev3_motor_set_power(left_motor,100);
-        ev3_motor_set_power(right_motor,-100);
+        ev3_motor_steer(
+            left_motor,
+            right_motor,
+            10,
+            20
+        );
     }
+    
     ev3_motor_stop(left_motor, false);
     ev3_motor_stop(right_motor, false);
-
+    ext_tsk;
 }
