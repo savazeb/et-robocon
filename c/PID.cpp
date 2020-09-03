fb012bcbff82db2096f86899b5cbf3a491eac081
savazeb <44660901+savazeb@users.noreply.github.com>
2020-09-03 10:49:55 +0900

    Update PID.cpp

--- c/PID.cpp
+++ c/PID.cpp
@@ -3,65 +3,85 @@
 
 using namespace std;
 
-class PID{
-    private:
-    void init(double P, double I, double D, double SetPoint);
-    void clear();
-    double update(double feedback_value , double current_time);
-    void setSampleTime(double sample_time);
-    
-    double Kp;
-    double Ki;
-    double Kd;
-    double current_time;
-    double sample_time;
-    double last_time;
-    double SetPoint_temp;
-    double PTerm;
-    double ITerm;
-    double DTerm;
-    double int_error;
-    double windup_guard;
-    double output;
-    double error;
-    double delta_time;
-    double delta_error;
-    double last_error;
-    
-};
-
-void PID::init(double P, double I, double D, double SetPoint){
+void PID::init(double P, double I, double D, double SP){
     
     Kp = P;
     Ki = I;
     Kd = D;
     
-    sample_time = 0.00;
+    SampleTime = 0.00;
     
     time_t ct;
     
     current_time = ct;
     last_time = current_time;
     
-    SetPoint_temp = SetPoint;
+    SetPoint = SP;
     
     clear();
 }
 
 void PID::clear(){
     
+    /*clears PID computations and coefficients*/
+    
     PTerm = 0.00;
     ITerm = 0.00;
     DTerm = 0.00;
-
+    last_error = 0.00;
+    
+    /*Windup Guard*/
+    int_error = 0.00;
+    windup_guard = 0.00;
+    
+    output = 0.00;
     
 }
 
+double PID::update(double feedback_value , double current_time){
+    error = SetPoint - (100 * (feedback_value - 4) / (44 - 4));
+    
+    time_t ct;
+    current_time = ct;
+    delta_time =  current_time - last_time;
+    delta_error = error - last_error;
+    
+    if (delta_time >= SampleTime){
+        PTerm = Kp * error;
+        ITerm += error * delta_time;
+        
+        if(ITerm < -windup_guard)
+            ITerm = -windup_guard;
+        else if(ITerm > windup_guard)
+            ITerm = windup_guard;
+        
+        DTerm = 0.00;
+        if(delta_time > 0)
+            DTerm = delta_error / delta_time;
+        
+        /*Remember last time and last error for next calculation*/
+        last_time = current_time;
+        last_error = error;
+        
+        output = PTerm + (Ki * ITerm) + (Kd * DTerm);
+        
+    }
+    
+    return output;
+}
+
+void PID::setSampleTime(double st){
+    SampleTime = st;
+}
 
 
 int main()
 {
-   cout << "Hello World" << endl; 
-   
-   return 0;
+    time_t ct;
+    double current = ct;
+    cout << current << endl;
+    double test = 1.00;
+    double test1 = 2;
+    if (test > -test1)
+        cout << test << endl;  return 0;
 }
